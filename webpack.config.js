a051376832c9f9be31793bd1b1425f8b15053899
Stepan Riha <github@nonplus.net>
2018-03-24 07:31:02 -0500

    add CSS production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const publicFolder = path.join(__dirname, 'public');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: publicFolder,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: publicFolder,
-    historyApiFallback: true
-  }
+module.exports = (env, argv) => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: publicFolder,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]            
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: publicFolder,
+      historyApiFallback: true
+    }
+  };
 };
